bbfb2499cbdda4f579d2d92b67e0985fa4e13361
NickTheFurbyConnectHacker <lilyofthevalleycookieshop@gmail.com>
2026-08-10 15:30:32 -0400

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -27,5 +27,14 @@
         "buttons": [
             { "title": "Chilli ", "action": [75,0,4,4] }
         ]
+    },
+        {
+        "file": "tu020510.dlc", 
+        "title": "Chilli",
+        "buttons": [
+            { "title": "Dlc1", "action": [75,0,0,0] },
+            { "title": "Dlc2", "action": [75,0,0,1] },
+            { "title": "Dlc3", "action": [75,0,0,2] }
+        ]
     }
 ]
